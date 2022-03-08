a7b4b56375f18be4fb31367b6e30c411057c235e
Petr Illek <petr@annertech.com>
2022-03-08 23:10:54 +0100

    Update style fort he dictionary accrodion trigger.

--- components/basecomponents/Collapse.tsx
+++ components/basecomponents/Collapse.tsx
@@ -10,12 +10,12 @@ interface CollapseProps {
 export const Collapse = ({ title, children }: CollapseProps): JSX.Element => {
   const [expanded, setExpanded] = useState(false);
   return (
-    <div className="bg-white  p-4 border-b-1 border-b-primary-grey ">
-      <div className="grid  grid-cols-[90%_10%] items-center " onClick={() => setExpanded(!expanded)}>
+    <div className="bg-white border-b-1 border-b-primary-grey ">
+      <div className="grid cursor-pointer p-4 grid-cols-[90%_10%] items-center " onClick={() => setExpanded(!expanded)}>
         <div>
           <p className="text-primary-blue text-lg font-bold">{title}</p>
         </div>
-        <div className="justify-self-end cursor-pointer">{expanded ? <ChevronDown /> : <ChevronRight />}</div>
+        <div className="justify-self-end cursor-pointer">{expanded ? <ChevronDown className="fill-primary-red" /> : <ChevronRight className="fill-primary-red" />}</div>
       </div>
       {children && expanded && <div className="my-2 ">{children}</div>}
     </div>
