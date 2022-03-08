aa54ce4ba0f58f545a6986396a4ea9722a0b8524
Oleg Antonyák <oleg.antonyak@dxheroes.io>
2022-03-08 22:27:57 +0100

    feat: added ua text for about page, responsivness for about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -16,16 +16,19 @@ const LinkText = ({ href, children, target }: { href: string; children?: string;
 
 const About = () => {
   const { t } = useTranslation();
+
+  const paragraph_style = 'mb-1 mt-5 sm:my-4 text-primary-blue';
+
   return (
     <div>
-      <h1 className="my-4 text-primary-blue">
+      <h1 className={`${paragraph_style} mb-6`}>
         <Trans className="block my-2">{t('about_page.title')}</Trans>
       </h1>
-      <h2 className="my-4 text-primary-blue">
+      <h2 className={paragraph_style}>
         <Trans>{t('about_page.movapp_goal_title')}</Trans>
       </h2>
       <p>{t('about_page.movapp_goal_description')}</p>
-      <h2 className="my-4 text-primary-blue">
+      <h2 className={paragraph_style}>
         <Trans>{t('about_page.why_movapp_title')}</Trans>
       </h2>
       <Trans
@@ -36,7 +39,7 @@ const About = () => {
           <LinkText href="https://drive.google.com/drive/u/0/folders/129vObZ0vUHpDd07slIfaiAfKsEbx1mNw" target="_blank" />,
         ]}
       />
-      <h2 className="my-4 text-primary-blue">
+      <h2 className={paragraph_style}>
         <Trans>{t('about_page.our_team_title')}</Trans>
       </h2>
       <Trans
@@ -44,7 +47,7 @@ const About = () => {
         t={t}
         components={[<LinkText href="https://www.movapp.cz/contacts" target="_blank" />]}
       />
-      <h2 className="my-4 text-primary-blue">
+      <h2 className={paragraph_style}>
         <Trans>{t('about_page.stand_with_ukraine_title')}</Trans>
       </h2>
       <Trans
@@ -52,7 +55,7 @@ const About = () => {
         t={t}
         components={[<LinkText href="https://stojimezaukrajinou.cz/" target="_blank" />]}
       />
-      <h2 className="my-4 text-primary-blue">
+      <h2 className={paragraph_style}>
         <Trans>{t('about_page.czech_digital_title')}</Trans>
       </h2>
       <Trans
