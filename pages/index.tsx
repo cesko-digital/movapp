f4ce61c4843a2bca320063a5d2493d77004b6f37
Petr Illek <petr@annertech.com>
2022-03-09 12:32:37 +0100

    Add content, style and images for homepage.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { useTranslation } from 'next-i18next';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 export { getStaticProps } from '../utils/localization';
+import DictionaryIcon from '../public/icons/book-font.svg';
+import MovappIcon from '../public/icons/movapp-bw-icon.svg';
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
@@ -12,7 +14,26 @@ const Home: NextPage = () => {
         <meta name="description" content={t('seo.homepage_page_description')} />
         <meta name="twitter:title" content={t('seo.homepage_page_title')} />
       </Head>
-      <div className="max-w-3xl m-auto">
+      <div className="bg-homepage-hero bg-center pt-20 pb-[10rem] pl-4 pr-4 bg-cover">
+        <h1 className="text-center max-w-3xl m-auto pt-12 pb-12 text-primary-blue text-[2.5rem] leading-snug">{t('homepage.title')}</h1>
+      </div>
+      <div className='max-w-7xl m-auto'>
+        <div className="max-w-7xl bg-white p-12 flex flex-wrap md:flex-nowrap shadow-xxl mt-[-6rem]">
+          <div className="md:w-2/6 mb-8 mr-8 pr-8 md:border-r-1 md:border-r-solid md:border-r-primary-grey">
+            <DictionaryIcon className="w-6 mb-2" />
+            <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">{t('homepage.box_dictionary_title')}</h2>
+            <p>{t('homepage.box_dictionary_description')}</p>
+          </div>
+          <div className="sm:w-4/6 md:w-3/6 mb-8 ">
+            <MovappIcon className="w-8 mb-2" />
+            <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">{t('homepage.box_movapp_title')}</h2>
+            <p className="mb-2">{t('homepage.box_movapp_description_top')}</p>
+            <p><em>{t('homepage.box_movapp_description_bottom')}</em></p>
+          </div>
+          <div className="sm:w-2/6 md:w-1/6 mb-8 flex justify-end">
+            <img src="/hearts-for-ukraine.png" className="self-center" />
+          </div>
+        </div>
       </div>
     </>
   );
