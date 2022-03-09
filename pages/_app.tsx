78cd25f150fdd0c1780ee14e3c4a48127b1ef001
Oleg Antonyák <oleg.antonyak@dxheroes.io>
2022-03-09 23:37:43 +0100

    Removed unneccasery font weights, replaced with fontsource package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,12 @@ import { ScrollToTop } from '../components/basecomponents/ScrollToTop';
 import { Layout } from '../components/sections/Layout/Layout';
 import { appWithTranslation } from 'next-i18next';
 
+// Fonts import
+import '@fontsource/roboto/200.css';
+import '@fontsource/roboto/600.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/800.css';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
