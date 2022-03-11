b9580e59a8613419453640910f2e34a63cf383bf
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-03-11 14:24:24 +0000

    Rafactor ExportTranslations.tsx some more

--- components/sections/ExportTranslations.tsx
+++ components/sections/ExportTranslations.tsx
@@ -2,6 +2,7 @@ import { Fragment, InputHTMLAttributes, LabelHTMLAttributes, useState } from 're
 import { Button } from '../basecomponents/Button';
 import { Translation } from '../basecomponents/TranslationContainer';
 import { Modal } from '../basecomponents/Modal';
+import Link from 'next/link';
 
 const PREVIEW_PHRASES_COUNT = 3;
 
@@ -32,16 +33,26 @@ const PHRASE_SEP_CUSTOM = 'phrase_custom';
 
 const RadioButton = ({ ...props }: InputHTMLAttributes<HTMLInputElement>) => <input type="radio" className="ml-3" {...props} />;
 const Label = ({ ...props }: LabelHTMLAttributes<HTMLLabelElement>) => <label className="ml-3" {...props} />;
+const TextInput = ({ ...props }: InputHTMLAttributes<HTMLInputElement>) => (
+  <input
+    type="text"
+    maxLength={50}
+    className="rounded-md md:rounded-lg max-w-full py-1 px-2 text-dark-700 border-1 border-primary-blue outline-none shadow-s"
+    {...props}
+  />
+);
+const Separator = () => (
+  <div className="pb-2">
+    <div className="w-full border-t border-gray-300"></div>
+  </div>
+);
 
 export const ExportTranslations = ({ translations, category }: ExportTranslationsProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [translationSeparator, setTranslationSeparator] = useState(TRANSLATION_SEPARATORS[0].value);
   const [customTranslationSeparator, setCustomTranslationSeparator] = useState('');
   const [phraseSeparator, setPhraseSeparator] = useState(PHRASE_SEPARATORS[0].value);
   const [customPhraseSeparator, setCustomPhraseSeparator] = useState('');
-
-  const closeModal = () => setIsOpen(false);
-  const openModal = () => setIsOpen(true);
 
   const translSep = translationSeparator === TRANS_SEP_CUSTOM ? customTranslationSeparator : translationSeparator;
   const phraseSep = phraseSeparator === PHRASE_SEP_CUSTOM ? customPhraseSeparator : phraseSeparator;
@@ -54,10 +65,10 @@ export const ExportTranslations = ({ translations, category }: ExportTranslation
 
   return (
     <>
-      <span onClick={openModal} className="cursor-pointer underline text-primary-blue ml-4 pb-4 inline-block">
+      <span onClick={() => setIsModalOpen(true)} className="cursor-pointer underline text-primary-blue ml-4 pb-4 inline-block">
         Download phrases
       </span>
-      <Modal closeModal={closeModal} isOpen={isOpen} title={'Download phrases'}>
+      <Modal closeModal={() => setIsModalOpen(false)} isOpen={isModalOpen} title={'Download phrases'}>
         <h3 className="my-4">Separator between the phrase and translation:</h3>
 
         {TRANSLATION_SEPARATORS.map((option) => (
@@ -87,20 +98,12 @@ export const ExportTranslations = ({ translations, category }: ExportTranslation
           {translationSeparator === TRANS_SEP_CUSTOM && (
             <>
               <span>:&nbsp;&nbsp;&nbsp;</span>
-              <input
-                type="text"
-                maxLength={50}
-                value={customTranslationSeparator}
-                className="rounded-md md:rounded-lg py-1 px-3 text-dark-700 border-1 border-primary-blue outline-none shadow-s"
-                onChange={(e) => setCustomTranslationSeparator(e.target.value)}
-              ></input>
+              <TextInput value={customTranslationSeparator} onChange={(e) => setCustomTranslationSeparator(e.target.value)} />
             </>
           )}
         </Label>
-        <br />
 
         <h3 className="my-4">Separator between phrases:</h3>
-
         {PHRASE_SEPARATORS.map((option) => (
           <>
             <RadioButton
@@ -127,30 +130,31 @@ export const ExportTranslations = ({ translations, category }: ExportTranslation
           Custom
           {phraseSeparator === PHRASE_SEP_CUSTOM && (
             <>
-              <span>:&nbsp;&nbsp;&nbsp; </span>
-              <input
-                type="text"
-                maxLength={50}
-                value={customPhraseSeparator}
-                className="rounded-md md:rounded-lg py-1 px-3 text-dark-700 border-1 border-primary-blue outline-none shadow-s"
-                onChange={(e) => setCustomPhraseSeparator(e.target.value)}
-              ></input>
+              <span>:&nbsp;&nbsp;&nbsp;</span>
+              <TextInput value={customPhraseSeparator} onChange={(e) => setCustomPhraseSeparator(e.target.value)} />
             </>
           )}
         </Label>
         <br />
 
         <h3 className="my-4">Preview:</h3>
-        <code className="whitespace-pre-wrap">{translationStrings.slice(0, PREVIEW_PHRASES_COUNT)}</code>
+        <div className="bg-gray-100 border-1 border-gray-400 p-2">
+          <code className="whitespace-pre-wrap">{translationStrings.slice(0, PREVIEW_PHRASES_COUNT)}</code>
+        </div>
 
-        <div>
+        <div className="flex justify-center">
           <a download={fileName} href={downloadLink}>
             <Button text="Download phrases" className="my-6" />
           </a>
         </div>
-        <div className="text-sm">
-          Obsah můžete pro své učely používat zdarma a bez omezení, šířit ho dál můžete jen za pomínek licence CC BY-NC 4.0
-          https://creativecommons.org/licenses/by-nc/4.0/deed.cs
+        <Separator />
+        <div className="text-sm font-light">
+          Obsah můžete pro své učely používat zdarma a bez omezení, šířit ho dál můžete jen za podmínek licence&nbsp;
+          <Link href={'https://creativecommons.org/licenses/by-nc/4.0/deed.cs'}>
+            <a target={'_blank'} className="underline">
+              CC BY-NC 4.0
+            </a>
+          </Link>
         </div>
       </Modal>
     </>
