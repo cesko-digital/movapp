8105ac82a5cfe00e0a9188b7ecb3b4c9fd7e2c16
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-03-11 10:54:25 +0000

    Modal in separate component

---
+++ components/basecomponents/Modal.tsx
@@ -0,0 +1,50 @@
+import { Transition, Dialog } from '@headlessui/react';
+import React, { Fragment, ReactNode } from 'react';
+
+interface ModalProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  title?: ReactNode;
+  closeModal: () => void;
+}
+
+export const Modal = ({ children, isOpen, title, closeModal }: ModalProps): JSX.Element => {
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={closeModal}>
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span className="inline-block h-screen align-middle" aria-hidden="true">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+              <Dialog.Title as="h2">{title}</Dialog.Title>
+              {children}
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
