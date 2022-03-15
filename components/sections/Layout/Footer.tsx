ee6b04b8668f1946d9d63c39c9da596fad45f3a6
Petr ILLEK <petr@annertech.com>
2022-03-15 16:24:02 +0100

    Change main colors to be in sync with other projects. (#9)
    * Change main colors to be in sync with other projects.
    
    * Update logo and favicons with new colors.
    
    * Recolor the footer.

--- components/sections/Layout/Footer.tsx
+++ components/sections/Layout/Footer.tsx
@@ -6,24 +6,24 @@ import { FOOTER_NAVIGATION } from '../../../data/footerNavigation';
 export const Footer = () => {
   const { t } = useTranslation();
   return (
-    <footer className="bg-primary-black">
+    <footer className="bg-primary-yellow">
       <div className="max-w-4xl m-auto p-2 sm:py-5 ">
-        <p className="text-primary-yellow text-center font-black text-xl sm:text-2xl mt-2 sm:mt-4 mb-2 sm:mb-6">#StandWithUkraine</p>
+        <p className="text-primary-black text-center font-black text-xl sm:text-2xl mt-2 sm:mt-4 mb-2 sm:mb-6">#StandWithUkraine</p>
         <div className="flex flex-col sm:flex-row justify-between items-center">
           {FOOTER_NAVIGATION.map(({ title, link, description }, index) => {
             return (
               <Link key={index} href={link}>
                 <a target={'_blank'} className="sm:w-2/6">
                   <div className="py-1">
-                    <p className="text-white text-center text-sm sm:text-base font-bold my-2">{title}</p>
-                    <p className="text-white text-center font-light text-xs sm:text-sm">{t(description)}</p>
+                    <p className="text-primary-black text-center text-sm sm:text-base font-bold my-2">{title}</p>
+                    <p className="text-primary-black text-center font-light text-xs sm:text-sm">{t(description)}</p>
                   </div>
                 </a>
               </Link>
             );
           })}
         </div>
-        <p className="text-white text-center text-xs pt-10">
+        <p className="text-primary-black text-center text-xs pt-10">
           Přidej se k vývoji na{' '}
           <Link href={'https://github.com/cesko-digital/movapp'}>
             <a target={'_blank'} className="underline">
