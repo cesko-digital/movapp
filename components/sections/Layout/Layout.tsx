12e66d6f1deb11f2bc1440569f4e2583133bfefb
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-03-16 19:57:54 +0000

    Use cs/uk in <html lang> instead of cz/ua

--- components/sections/Layout/Layout.tsx
+++ components/sections/Layout/Layout.tsx
@@ -1,12 +1,24 @@
 import Script from 'next/script';
 import { Footer } from './Footer';
 import Header from './Header/index';
+import { useEffect } from 'react';
+import { useTranslation } from 'next-i18next';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
+  /**
+   *   This overwrites the html lang attribute from our non-standard locale naming (ua/cz) to the correct ISO 639-1 language code (uk/cs).
+   *   Happens on every rerender of Layout. The alternative is to change our locale names to uk/cs (would also affect routes,
+   *   might as well change it everywhere for consistency at that point).
+   */
+  const { i18n } = useTranslation('common');
+  useEffect(() => {
+    document.documentElement.setAttribute('lang', i18n.language === 'cz' ? 'cs' : 'uk');
+  });
+
   return (
     <div>
       <Header />
