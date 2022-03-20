93d735f573baf14595c019b9585c5813dbf800cc
baryska <barbora.skalova@newtontech.cz>
2022-03-20 19:39:23 +0100

    fix layout of homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,23 +23,27 @@ const Home: NextPage = () => {
           {t('homepage.title')}
         </h1>
       </div>
-      <div className="max-w-7xl m-auto">
-        <div className="max-w-7xl bg-white p-4 sm:p-8 md:p-12 grid  md:grid-cols-[35%_50%_15%]  md:flex-nowrap shadow-xxl mt-[-6rem]">
-          <div className="homepage-box w-full group hover:text-primary-blue    pr-4 md:border-r-1 md:border-r-solid md:border-r-primary-grey">
-            <DictionaryIcon className="w-6 mb-2 group-hover:fill-primary-red" />
-            <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">
-              <Link href={'/dictionary'}>{t('homepage.box_dictionary_title')}</Link>
-            </h2>
-            <p>{t('homepage.box_dictionary_description')}</p>
-          </div>
-          <div className="homepage-box w-full group hover:text-primary-blue    pr-4 md:border-r-1 md:border-r-solid md:border-r-primary-grey">
-            <ChildIcon className="w-6 mb-2 group-hover:fill-primary-red" />
-            <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">
-              <Link href={'/kids'}>
-                <a>{t('homepage.box_child_title')}</a>
-              </Link>
-            </h2>
-            <p>{t('homepage.box_child_description')}</p>
+      <div className="max-w-7xl m-auto px-2 sm:px-4">
+        <div className="max-w-7xl bg-white p-4 sm:p-8 md:p-12 shadow-xxl mt-[-6rem]">
+          <div className="grid md:grid-cols-3 gap-8">
+            <div className="homepage-box w-full group hover:text-primary-blue    pr-4 md:border-r-1 md:border-r-solid md:border-r-primary-grey">
+              <DictionaryIcon className="w-6 mb-2 group-hover:fill-primary-red" />
+              <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">
+                <Link href={'/dictionary'}>
+                  <a>{t('homepage.box_dictionary_title')}</a>
+                </Link>
+              </h2>
+              <p>{t('homepage.box_dictionary_description')}</p>
+            </div>
+            <div className="homepage-box w-full group hover:text-primary-blue    pr-4 md:border-r-1 md:border-r-solid md:border-r-primary-grey">
+              <ChildIcon className="w-6 mb-2 group-hover:fill-primary-red" />
+              <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">
+                <Link href={'/kids'}>
+                  <a>{t('homepage.box_child_title')}</a>
+                </Link>
+              </h2>
+              <p>{t('homepage.box_child_description')}</p>
+            </div>
           </div>
           <div className="grid md:grid-cols-[70%_30%] gap-8 mt-10">
             <div className="homepage-box group hover:text-primary-blue my-6 md:my-0">
