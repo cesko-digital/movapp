369a38f1b8d2c294d60ba50c4cb3db761c8454f5
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-03-23 22:17:27 +0000

    Merge main

--- components/sections/Layout/Layout.tsx
+++ components/sections/Layout/Layout.tsx
@@ -9,12 +9,12 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <div>
+    <>
       <Header />
       <Script data-domain="movapp.cz" src="https://plausible.io/js/plausible.js" />
       <main className="bg-white sm:bg-primary-grey pt-2 pb-5 min-h-screen px-2 m-auto">{children}</main>
       <Footer />
       <ScrollToTop />
-    </div>
+    </>
   );
 };
