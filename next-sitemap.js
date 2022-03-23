d7d4c1aed57cf38a8cdbb6a40e4914561cdab08a
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-03-23 22:27:13 +0000

    Include WWW in site URL

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: 'https://movapp.cz',
+  siteUrl: 'https://www.movapp.cz',
   generateRobotsTxt: true,
   exclude: ['/404', '/uk/404'],
 };
