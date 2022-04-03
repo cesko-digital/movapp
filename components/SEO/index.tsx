d795db6de0fd782a1f2b51925d80578e27a3ec88
baryska <barbora.skalova@newtontech.cz>
2022-04-03 11:48:13 +0200

    fix image meta tags

--- components/SEO/index.tsx
+++ components/SEO/index.tsx
@@ -14,11 +14,11 @@ const SEO = ({ title, description, image }: Props) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="name" content={title} />
-      <meta name="image" property="og:image" content={image} />
-      <meta name="og:title" content={title} />
-      <meta name="og: description" content={description} />
+      <meta property="og:image" content={image} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content={image} />
       <meta name="twitter:title" content={title} />
