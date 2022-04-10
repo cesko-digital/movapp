c65abd11c4a6c19356167b20bdb65c2748bd1015
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-04-10 17:37:55 +0100

    Try building again

--- utils/transliterate.ts
+++ utils/transliterate.ts
@@ -1,6 +1,7 @@
 import { ua2cz } from 'data/transliterations/ua2cz';
 import { cz2ua } from './../data/transliterations/cz2ua';
 export const translit = (subs: [string, string][], text: string) => {
+  console.log(text);
   let result = text;
   for (let i = 0; i < subs.length; i++) {
     result = result.replaceAll(subs[i][0], subs[i][1]);
