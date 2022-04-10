d41f8a5278ad2f2c3d6f53ada22fb3ed6421de32
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-04-10 18:19:15 +0100

    Fix transliteration

--- utils/transliterate.ts
+++ utils/transliterate.ts
@@ -1,12 +1,15 @@
 import { ua2cz } from 'data/transliterations/ua2cz';
 import { cz2ua } from './../data/transliterations/cz2ua';
+
+const removePunctuation = (str: string) => str.replace('.', '').replace('?', '').replace('!', '');
+
 export const translit = (subs: [string, string][], text: string) => {
-  console.log(text);
   let result = text;
   for (let i = 0; i < subs.length; i++) {
-    result = result.replaceAll(subs[i][0], subs[i][1]);
+    // Vercel's Node 14 does not support String.replaceAll(), so we must use a RegEx workaround
+    result = result.replace(new RegExp(subs[i][0], 'g'), subs[i][1]);
   }
-  return result.replaceAll('.', '').replaceAll('?', '').replaceAll('!', '');
+  return removePunctuation(result);
 };
 
 export const translitFromUkrainian = (text: string) => translit(ua2cz, text);
