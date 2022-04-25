6168083626f8b96b7a2f1b7f19bef9c92a1f4006
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-04-25 22:24:08 +0100

    Include only the necessary locales

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
+const mainLanguage = process.env.NEXT_PUBLIC_COUNTRY_VARIANT ?? 'cs';
+
 module.exports = {
   i18n: {
-    locales: ['cs', 'uk', 'sk'],
+    locales: ['uk', mainLanguage],
     // eslint-disable-next-line no-process-env
-    defaultLocale: process.env.NEXT_PUBLIC_COUNTRY_VARIANT ?? 'cs',
+    defaultLocale: mainLanguage,
     localePath: path.resolve('./public/locales'),
     localeDetection: false,
   },
