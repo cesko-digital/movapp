d694fc8866ff932779ec2ee58d8c4898f6f03347
Marek Mego <marekmego84@gmail.com>
2022-05-02 11:27:08 +0200

    country variant update

--- pages/kids/memory-game/index.tsx
+++ pages/kids/memory-game/index.tsx
@@ -1,10 +1,22 @@
 import { useTranslation } from 'next-i18next';
 import React from 'react';
-import kidsWords from 'data/translations/pro-deti.json';
+import kidsWords_CZ from 'data/translations/cs/pro-deti.json';
+import kidsWords_SK from 'data/translations/sk/pro-deti_sk.json';
+import kidsWords_PL from 'data/translations/pl/pro-deti_pl.json';
 import dynamic from 'next/dynamic';
 import SEO from 'components/basecomponents/SEO';
+import { TranslationJSON } from 'utils/Phrase';
+import { CountryVariant, getCountryVariant } from 'utils/locales';
 import MemoryGameLoading from 'components/basecomponents/MemoryGame/MemoryGameLoading';
 export { getStaticProps } from 'utils/localization';
+
+type KidsTranlsation = TranslationJSON & { image: string };
+
+const KIDS_WORDS: Record<CountryVariant, KidsTranlsation[]> = {
+  cs: kidsWords_CZ,
+  sk: kidsWords_SK,
+  pl: kidsWords_PL,
+};
 
 const MemoryGame = dynamic(() => import('components/basecomponents/MemoryGame/MemoryGame'), {
   ssr: false,
@@ -19,7 +31,7 @@ const normalizeData = ({ main, uk, image }: { main: string; uk: string; image: s
   image,
 });
 
-const cardsData = kidsWords.map(normalizeData);
+const cardsData = KIDS_WORDS[getCountryVariant()].map(normalizeData);
 
 const MemoryGameSection = () => {
   const { t } = useTranslation();
