718b39d7031e14f16ae56bb5433aac2a8b6cd363
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-05-05 07:59:42 +0200

    Add global analytics tracking profile (#100)
    * Add global analytics tracking profile
    
    * Add country specific plausible domains
    
    Co-authored-by: Juraj Uhlar <“juraj.uhlar@flowmon.com”>

--- components/sections/Layout/Layout.tsx
+++ components/sections/Layout/Layout.tsx
@@ -2,6 +2,13 @@ import Script from 'next/script';
 import { Footer } from './Footer';
 import Header from './Header/index';
 import { ScrollToTop } from 'components/basecomponents/ScrollToTop';
+import { CountryVariant, getCountryVariant } from 'utils/locales';
+
+const PLAUSIBLE_DOMAINS: Record<CountryVariant, string> = {
+  cs: 'movapp.cz, all.movapp.eu',
+  sk: 'sk.movapp.eu, all.movapp.eu',
+  pl: 'pl.movapp.eu, all.movapp.eu',
+};
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,7 +18,7 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <>
       <Header />
-      <Script data-domain="movapp.cz" src="https://plausible.io/js/plausible.js" />
+      <Script data-domain={PLAUSIBLE_DOMAINS[getCountryVariant()]} src="https://plausible.io/js/plausible.js" />
       <main className="bg-white sm:bg-primary-grey pt-2 pb-5 min-h-screen px-2 m-auto">{children}</main>
       <Footer />
       <ScrollToTop />
