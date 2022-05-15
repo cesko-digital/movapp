752c325378a67214c33c0d95a36d80de3b67e18c
Martin Hassman <martin.hassman@gmail.com>
2022-05-15 19:46:15 +0200

    Appstore link with campain (#117)

--- components/basecomponents/DownloadAppStore.tsx
+++ components/basecomponents/DownloadAppStore.tsx
@@ -6,7 +6,7 @@ export const DownloadAppStore = () => {
   return (
     <div className="mx-auto mb-12 mt-20 flex justify-center align-middle">
       <a
-        href="https://apps.apple.com/app/movapp/id1617768476"
+        href="https://apps.apple.com/app/apple-store/id1617768476?pt=124252508&ct=web-movappcz&mt=8"
         target="_blank"
         rel="noreferrer"
         className="bg-black text-white px-3 py-2 rounded-xl flex"
