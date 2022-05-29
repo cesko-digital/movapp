a642d13822dcee3ad36e5590184cb74363b325c1
baryska <barbora.skalova@newtontech.cz>
2022-05-29 20:14:24 +0200

    rename stories slugs and images

--- data/stories.ts
+++ data/stories.ts
@@ -1,37 +1,37 @@
 const stories = [
   {
     title: { cs: 'O perníkové chaloupce', uk: 'Прянична хатинка' },
-    slug: 'chaloupka',
+    slug: 'pernikova-chaloupka',
     duration: '3 min',
     country: 'CZ',
   },
   {
     title: { cs: 'O dvanácti měsíčkách', uk: 'Дванадцять місяців' },
-    slug: 'mesicky',
+    slug: 'dvanact-mesicku',
     duration: '3 min',
     country: 'CZ',
   },
   {
     title: { cs: 'O červené karkulce', uk: 'Червона Шапочка' },
-    slug: 'karkulka',
+    slug: 'cervena-karkulka',
     duration: '3 min',
     country: 'CZ',
   },
   {
     title: { cs: 'Ivasik-Telesik', uk: 'Івасик-Телесик' },
-    slug: 'ivasik',
+    slug: 'ivasik-telesik',
     duration: '4 min',
     country: 'UA',
   },
   {
     title: { cs: 'O koblížkovi', uk: 'Колобок' },
-    slug: 'koblizek',
+    slug: 'kolobok',
     duration: '3 min',
     country: 'UA',
   },
   {
     title: { cs: 'Husy-labutě', uk: 'Гуси-Лебеді' },
-    slug: 'husy',
+    slug: 'husy-lebedi',
     duration: '3 min',
     country: 'UA',
   },
