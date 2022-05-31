44d625f17b205aaa98c84180a6367ac04fb8019b
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-05-31 20:51:44 +0100

    Revert "Try one last time"
    This reverts commit 7fc97996ddc3b9ec69faf9741d3fd5e7067e3215.

--- components/basecomponents/MemoryGame/MemoryGameCore.tsx
+++ components/basecomponents/MemoryGame/MemoryGameCore.tsx
@@ -62,156 +62,27 @@ interface MemoryGameProps {
   cardsData: CardData[];
   cardBackImage: string;
 }
-interface SelectedCards {
-  first: Card | null;
-  second: Card | null;
-}
 
 const MemoryGame = ({ cardsData, audio, styles, cardBackImage }: MemoryGameProps) => {
   const { playCardPhraseOtherLang, playCardPhraseCurrentLang, playPhraseRandomLang } = usePlayPhrase();
   const { t } = useTranslation();
 
   const [cards, setCards] = useState<Card[]>([]);
-  const [selectedCards, setSelectedCards] = useState<SelectedCards>({ first: null, second: null });
 
-  const [scene, setScene] = useState<Scene | null>(null);
-  const [controlsDisabled, setControlsDisabled] = useState<boolean>(true);
-  const [setTimer, clearTimers] = useMemo(createTimer, []);
+  interface SelectedCards {
+    first: Card | null;
+    second: Card | null;
+  }
+
+  const [selectedCards, setSelectedCards] = useState<SelectedCards>({ first: null, second: null });
 
   const isSelected = (card: Card) =>
     (selectedCards.first !== null && card.id === selectedCards.first.id) ||
     (selectedCards.second !== null && card.id === selectedCards.second.id);
 
-  useEffect(() => {
-    changeSceneTo(Scene.init);
-  }, []);
-
-  const sceneActions: Record<Scene, () => void> = {
-    init: () => {
-      // begin new game automaticaly
-      changeSceneTo(Scene.begin);
-    },
-    begin: () => {
-      // new game
-      newGame();
-      // disable controls
-      setControlsDisabled(true);
-      // play css animations
-      setTimer(() => {
-        changeSceneTo(Scene.game);
-      }, 1000);
-    },
-    game: () => {
-      // enable controls
-      setControlsDisabled(false);
-    },
-    firstCardSelected: () => {
-      // disable controls
-      setControlsDisabled(true);
-      // play css animations and sounds
-      const { first: card } = selectedCards;
-      flipCard(card!); // 0.3s
-      playCardPhraseOtherLang(card!);
-
-      setTimer(() => {
-        changeSceneTo(Scene.game);
-      }, 600); // reduced for fastrer UX
-    },
-    secondCardSelected: () => {
-      // disable controls
-      setControlsDisabled(true);
-      // play css animations and sounds
-      const { second: card } = selectedCards;
-      flipCard(card!); // 0.3s
-
-      playCardPhraseCurrentLang(card!).then(() => {
-        changeSceneTo(Scene.resolveCards);
-      });
-    },
-    resolveCards: () => {
-      const { first, second } = selectedCards;
-      const cardsMatch = first?.image === second?.image;
-
-      if (cardsMatch) {
-        console.log('cards match');
-        changeSceneTo(Scene.cardsMatch);
-      } else {
-        console.log('cards dont match');
-        changeSceneTo(Scene.cardsDontMatch);
-      }
-    },
-    cardsMatch: () => {
-      // disable controls
-      setControlsDisabled(true);
-      // play css animations and sounds
-      // cardsMatch animation 0.7s
-      setTimer(() => cardsMatchSound.play(), 100); // sync to animation
-
-      setTimer(() => {
-        changeSceneTo(Scene.cardsMatchReward);
-      }, 700);
-    },
-    cardsMatchReward: () => {
-      // play css animations and sounds
-      if (Math.random() > 0.5) {
-        playPhraseRandomLang(getRandomElement(phrases.good));
-      }
-      // reset selected cards
-      setSelectedCards({ first: null, second: null });
-      // check win
-      setTimer(() => {
-        if (cards.every((card) => card.flipped)) {
-          console.log('win');
-          changeSceneTo(Scene.win);
-        } else {
-          changeSceneTo(Scene.game);
-        }
-      }, 700);
-    },
-    cardsDontMatch: () => {
-      // disable controls
-      setControlsDisabled(true);
-      // play animations and sounds
-      setTimeout(() => {
-        if (Math.random() > 0.8) {
-          playPhraseRandomLang(getRandomElement(phrases.wrong));
-        }
-
-        // setTimer: show cards for some time to remember then flip back
-        const { first, second } = selectedCards;
-        flipCard(first!);
-        flipCard(second!);
-        setSelectedCards({ first: null, second: null });
-        changeSceneTo(Scene.cardsDontMatchFlipBack);
-      }, 1000);
-    },
-    cardsDontMatchFlipBack: () => {
-      // wait for cards flip back
-      setTimer(() => {
-        changeSceneTo(Scene.game);
-      }, 300);
-    },
-    win: () => {
-      // disable controls
-      setControlsDisabled(true);
-      // play css animations and sounds
-      playPhraseRandomLang(getRandomElement(phrases.win)).then(() => {
-        changeSceneTo(Scene.winReward);
-      });
-    },
-    winReward: () => {
-      // play css animations and sounds
-      winMusic.play();
-    },
-    goNewGame: () => {
-      clearTimers();
-      // play css animations and sounds
-      playPhraseRandomLang(getRandomElement(phrases.newGame));
-      setTimer(() => {
-        changeSceneTo(Scene.begin);
-      }, 500);
-    },
-  };
+  const [scene, setScene] = useState<Scene>(Scene.init);
+  const [controlsDisabled, setControlsDisabled] = useState<boolean>(true);
+  const [setTimer, clearTimers] = useMemo(createTimer, []);
 
   const { cardFlipSound, cardsMatchSound, winMusic } = useMemo(() => {
     const cardFlipSound = new Audio(audio.cardFlipSound);
@@ -253,35 +124,164 @@ const MemoryGame = ({ cardsData, audio, styles, cardBackImage }: MemoryGameProps
   const selectCard = (card: Card) => {
     if (controlsDisabled) return;
 
-    const nextCards = { ...selectedCards };
-    if (selectedCards.first === null && !card.flipped) {
-      nextCards.first = card;
-    } else if (selectedCards.second === null && !card.flipped) {
-      nextCards.second = card;
+    // Test debug
+    playPhraseRandomLang(getRandomElement(phrases.good));
+
+    const { first, second } = selectedCards;
+    if (first === null && !card.flipped) {
+      setSelectedCards({ ...selectedCards, first: card });
+    } else if (second === null && !card.flipped) {
+      setSelectedCards({ ...selectedCards, second: card });
     }
-    setSelectedCards(nextCards);
   };
 
   // handle selected cards
   useEffect(() => {
     const { first, second } = selectedCards;
+
     const bothCardsSelected = first !== null && second !== null;
 
     if (bothCardsSelected) {
-      changeSceneTo(Scene.secondCardSelected);
+      setScene(Scene.secondCardSelected);
       return;
     }
     if (first !== null) {
-      changeSceneTo(Scene.firstCardSelected);
+      setScene(Scene.firstCardSelected);
     }
   }, [selectedCards]);
 
-  const changeSceneTo = (scene: Scene) => {
+  // resolve game states
+  useEffect(() => {
+    const sceneActions: Record<Scene, () => void> = {
+      init: () => {
+        // begin new game automaticaly
+        setScene(Scene.begin);
+      },
+      begin: () => {
+        // new game
+        newGame();
+        // disable controls
+        setControlsDisabled(true);
+        // play css animations
+        setTimer(() => {
+          setScene(Scene.game);
+        }, 1000);
+      },
+      game: () => {
+        // enable controls
+        setControlsDisabled(false);
+      },
+      firstCardSelected: () => {
+        // disable controls
+        setControlsDisabled(true);
+        // play css animations and sounds
+        const { first: card } = selectedCards;
+        flipCard(card!); // 0.3s
+        playCardPhraseOtherLang(card!);
+
+        setTimer(() => {
+          setScene(Scene.game);
+        }, 600); // reduced for fastrer UX
+      },
+      secondCardSelected: () => {
+        // disable controls
+        setControlsDisabled(true);
+        // play css animations and sounds
+        const { second: card } = selectedCards;
+        flipCard(card!); // 0.3s
+
+        playCardPhraseCurrentLang(card!).then(() => {
+          setScene(Scene.resolveCards);
+        });
+      },
+      resolveCards: () => {
+        const { first, second } = selectedCards;
+        const cardsMatch = first?.image === second?.image;
+
+        if (cardsMatch) {
+          console.log('cards match');
+          setScene(Scene.cardsMatch);
+        } else {
+          console.log('cards dont match');
+          setScene(Scene.cardsDontMatch);
+        }
+      },
+      cardsMatch: () => {
+        // disable controls
+        setControlsDisabled(true);
+        // play css animations and sounds
+        // cardsMatch animation 0.7s
+        setTimer(() => cardsMatchSound.play(), 100); // sync to animation
+
+        setTimer(() => {
+          setScene(Scene.cardsMatchReward);
+        }, 700);
+      },
+      cardsMatchReward: () => {
+        // play css animations and sounds
+        if (Math.random() > 0.5) {
+          playPhraseRandomLang(getRandomElement(phrases.good));
+        }
+        // reset selected cards
+        setSelectedCards({ first: null, second: null });
+        // check win
+        setTimer(() => {
+          if (cards.every((card) => card.flipped)) {
+            console.log('win');
+            setScene(Scene.win);
+          } else {
+            setScene(Scene.game);
+          }
+        }, 700);
+      },
+      cardsDontMatch: () => {
+        // disable controls
+        setControlsDisabled(true);
+        // play animations and sounds
+        setTimeout(() => {
+          if (Math.random() > 0.8) {
+            playPhraseRandomLang(getRandomElement(phrases.wrong));
+          }
+
+          // setTimer: show cards for some time to remember then flip back
+          const { first, second } = selectedCards;
+          flipCard(first!);
+          flipCard(second!);
+          setSelectedCards({ first: null, second: null });
+          setScene(Scene.cardsDontMatchFlipBack);
+        }, 1000);
+      },
+      cardsDontMatchFlipBack: () => {
+        // wait for cards flip back
+        setTimer(() => {
+          setScene(Scene.game);
+        }, 300);
+      },
+      win: () => {
+        // disable controls
+        setControlsDisabled(true);
+        // play css animations and sounds
+        playPhraseRandomLang(getRandomElement(phrases.win)).then(() => {
+          setScene(Scene.winReward);
+        });
+      },
+      winReward: () => {
+        // play css animations and sounds
+        winMusic.play();
+      },
+      goNewGame: () => {
+        clearTimers();
+        // play css animations and sounds
+        playPhraseRandomLang(getRandomElement(phrases.newGame));
+        setTimer(() => {
+          setScene(Scene.begin);
+        }, 500);
+      },
+    };
     console.log(`scene is: ${scene}`);
     // run scene actions
     sceneActions[scene]();
-    setScene(scene);
-  };
+  }, [scene]);
 
   // clear timers on unmount
   useEffect(() => {
@@ -292,14 +292,14 @@ const MemoryGame = ({ cardsData, audio, styles, cardBackImage }: MemoryGameProps
 
   return (
     <div className={styles.app}>
-      <Button className={styles.newGameButton} text={t('utils.new_game')} onClick={() => changeSceneTo(Scene.goNewGame)} />
+      <Button className={styles.newGameButton} text={t('utils.new_game')} onClick={() => setScene(Scene.goNewGame)} />
       <div className={styles.board}>
         {cards.map((card) => (
           <Card
             key={card.id}
             onClick={selectCard}
             card={card}
-            scene={scene!}
+            scene={scene}
             styles={styles}
             selected={isSelected(card)}
             cardBackImage={cardBackImage}
