d47c74978561b8566c41250d57aaac8d446e32ed
Juraj Uhlar <juro.uhlar@gmail.com>
2022-06-03 17:30:51 +0100

    Add android app button (#149)
    Co-authored-by: Juraj Uhlar <“juraj.uhlar@flowmon.com”>

--- pages/apps.tsx
+++ pages/apps.tsx
@@ -2,7 +2,8 @@ export { getStaticProps } from '../utils/localization';
 import { useTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { DownloadAppStore } from '../components/basecomponents/DownloadAppStore';
+import { DownloadAppStore, DOWNLOAD_IOS_URL } from '../components/basecomponents/DownloadAppStore';
+import { DownloadGooglePlay, DOWNLOAD_ANDROID_URL } from '../components/basecomponents/DownloadGooglePlay';
 
 const MobileAppsPage = () => {
   const { t } = useTranslation();
@@ -11,19 +12,22 @@ const MobileAppsPage = () => {
   useEffect(() => {
     const userAgent = navigator.userAgent || navigator.vendor;
 
-    /* if (/android/i.test(userAgent)) {
-    } */
+    if (/android/i.test(userAgent)) {
+      router.push(DOWNLOAD_ANDROID_URL);
+    }
 
     if (/iPad|iPhone|iPod/i.test(userAgent)) {
-      router.push('https://apps.apple.com/app/apple-store/id1617768476?pt=124252508&ct=web-movappcz&mt=8');
+      router.push(DOWNLOAD_IOS_URL);
     }
-  }, []);
+  }, [router]);
 
   return (
     <div className="flex justify-center flex-col items-center text-center flex-grow">
       <h1 className="mb-8">{t('apps.download_app')}</h1>
-      <DownloadAppStore />
-      <h2 className="mt-10">{t('apps.android_soon')}</h2>
+      <div className="flex justify-center items-center flex-wrap">
+        <DownloadAppStore />
+        <DownloadGooglePlay />
+      </div>
     </div>
   );
 };
