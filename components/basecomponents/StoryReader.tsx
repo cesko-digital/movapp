010a4e00a161d7a535631b101f02aac542e5712d
Jakub Kolebaba <jakub@kolebaba.cz>
2022-07-20 19:43:41 +1000

    Fix story reader layout on mobile (#165)
    * Fix story reader layout on mobile
    
    * Smaller gaps and UI elements

--- components/basecomponents/StoryReader.tsx
+++ components/basecomponents/StoryReader.tsx
@@ -82,59 +82,59 @@ const StoryReader = ({ titleCurrent, titleOther, id, country }: StoryReaderProps
 
   return (
     <div className="w-full">
-      <div className="controls max-h-[20vh]">
-        <div className="flex items-center">
-          <h2>
+      <div className="controls">
+        <div className="flex items-center justify-between">
+          <h2 className="p-0 m-0 text-sm sm:text-base md:text-xl">
             {titleCurrent} / {titleOther}
           </h2>
-          <button onClick={() => handleLanguageChange('cs')}>
-            <Flag
-              language="cs"
-              width={40}
-              height={40}
-              className={`mr-3 ml-6 ease-in-out duration-300 ${languagePlay === 'cs' ? 'scale-125' : ''}`}
-            />
-          </button>
-          <button onClick={() => handleLanguageChange('uk')}>
-            <Flag
-              language="uk"
-              width={40}
-              height={40}
-              className={`mr-3 ease-in-out duration-300 ${languagePlay === 'uk' ? 'scale-125' : ''}`}
-            />
-          </button>
+          <div className="flex items-center">
+            <button onClick={() => handleLanguageChange('cs')}>
+              <Flag
+                language="cs"
+                width={27}
+                height={27}
+                className={`mr-3 ml-3 ease-in-out duration-300 ${languagePlay === 'cs' ? 'scale-125' : ''}`}
+              />
+            </button>
+            <button onClick={() => handleLanguageChange('uk')}>
+              <Flag
+                language="uk"
+                width={27}
+                height={27}
+                className={`ease-in-out duration-300 ${languagePlay === 'uk' ? 'scale-125' : ''}`}
+              />
+            </button>
+          </div>
         </div>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-between pt-2">
           <div className="flex">
             <button onClick={() => (isPlaying ? pauseStory() : playStory())}>
               {isPlaying ? (
-                <PauseIcon className="cursor-pointer active:scale-75 transition-all duration-300 mr-1.5" width="60" height="60" />
+                <PauseIcon className="cursor-pointer active:scale-75 transition-all duration-300 mr-1.5" width="50" height="50" />
               ) : (
-                <PlayIcon className="cursor-pointer active:scale-75 transition-all duration-300 mr-1.5" width="60" height="60" />
+                <PlayIcon className="cursor-pointer active:scale-75 transition-all duration-300 mr-1.5" width="50" height="50" />
               )}
             </button>
             <button onClick={() => stopStory()}>
               {' '}
-              <StopIcon className="cursor-pointer active:scale-75 transition-all duration-300" width="60" height="60" />
+              <StopIcon className="cursor-pointer active:scale-75 transition-all duration-300" width="50" height="50" />
             </button>
           </div>
-          <div className="w-10/12">
-            <input
-              type="range"
-              min="0"
-              max="100"
-              className="w-11/12 ml-6"
-              step="1"
-              value={seekValue}
-              onChange={(e) => {
-                const seekto = audio.current !== null ? audio.current.duration * (Number(e.target.value) / 100) : 0;
-                audio.current !== null ? (audio.current.currentTime = seekto) : null;
-                setSeekValue(Number(e.target.value));
-              }}
-            />
-          </div>
+          <input
+            type="range"
+            min="0"
+            max="100"
+            className="w-full ml-2 mr-2"
+            step="1"
+            value={seekValue}
+            onChange={(e) => {
+              const seekto = audio.current !== null ? audio.current.duration * (Number(e.target.value) / 100) : 0;
+              audio.current !== null ? (audio.current.currentTime = seekto) : null;
+              setSeekValue(Number(e.target.value));
+            }}
+          />
+          <p className="text-xl text-right">{time}</p>
         </div>
-        <p className="mr-2/10 text-xl text-right">{time}</p>
       </div>
       <div className="md:flex">
         {country === 'CZ' ? (
