2b291c543cfe8870675acaced29dafa2c717d34d
Michael Kubin <michael.kubn@gmail.com>
2022-09-01 14:19:06 +0200

    add: localization key

--- pages/about/team/index.tsx
+++ pages/about/team/index.tsx
@@ -26,6 +26,31 @@ interface Teams {
   members: string;
 }
 
+// Static dictionary is copy from https://data.movapp.eu/team.json, if you will want to add some team edit this dictionary
+enum TeamNames {
+  Ios = 'V\u00FDvoj Apple aplikace',
+  Android = 'V\u00FDvoj Android aplikace',
+  Web = 'V\u00FDvoj webu',
+  Leadership = 'Veden\u00ED projektu',
+  Marketing = 'Marketing',
+  ContentCreation = 'Tvorba obsahu',
+  Design = 'UX t\u00FDm',
+  Testing = 'Testov\u00E1n\u00ED',
+  Acknowledgements = 'D\u011Bkujeme t\u00FDmu z \u010Cesk\u00E9 spo\u0159itelny',
+}
+
+const getLocalizationKey: Record<string, string> = {
+  [TeamNames.Ios]: 'team.ios',
+  [TeamNames.Android]: 'team.android',
+  [TeamNames.Web]: 'team.web',
+  [TeamNames.Leadership]: 'team.leadership',
+  [TeamNames.Marketing]: 'team.marketing',
+  [TeamNames.ContentCreation]: 'team.content_creation',
+  [TeamNames.Design]: 'team.design',
+  [TeamNames.Testing]: 'team.testing',
+  [TeamNames.Acknowledgements]: 'team.acknowledgements',
+};
+
 // TODO translations
 const Team: NextPageWithLayout<{ teams: Teams[] }> = ({ teams }) => {
   const { t } = useTranslation();
@@ -38,12 +63,12 @@ const Team: NextPageWithLayout<{ teams: Teams[] }> = ({ teams }) => {
         description={t('seo.about_page_description')}
         image="https://www.movapp.cz/icons/movapp-cover.jpg"
       />
-        
+
       <H2>{t('about_page.our_team_title')}</H2>
 
       {teams.map(({ team, members }) => (
         <React.Fragment key={team}>
-          <H2>{team}</H2>
+          <H2>{t(team)}</H2>
           <P className="inline-block">{members}</P>
         </React.Fragment>
       ))}
@@ -75,7 +100,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
   }
 
   const pluck: Teams[] = teams.sections.map(({ name, members }) => ({
-    team: name,
+    team: getLocalizationKey[name],
     members: members.map(({ name }) => name).join(', '),
   }));
 
