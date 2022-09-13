48de96794cd24b715c1a3a18a667554477016fb5
Petr Hajek <petr@mywallst.com>
2022-09-13 18:43:19 +0200

    add plausible tracking using a library

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,16 @@
 export { getStaticProps } from '../utils/localization';
 import { useTranslation } from 'next-i18next';
+import { usePlausible } from 'next-plausible';
 import NextErrorComponent from 'next/error';
+import { useEffect } from 'react';
 
 const Custom404 = () => {
   const { t } = useTranslation();
+  const plausible = usePlausible();
+  useEffect(() => {
+    plausible('404', { props: { path: document.location.pathname } });
+  }, [plausible]);
+
   return <NextErrorComponent statusCode={404} title={t('errors.pageNotFound')} />;
 };
 
