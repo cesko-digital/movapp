4cf9ea9e87c7dc86388ecfbc2e2381281d6af191
petr-hajek <38785076+petr-hajek@users.noreply.github.com>
2022-09-18 22:49:43 +0200

    Show scroll to top only when user has scrolled down (#179)

--- components/basecomponents/ScrollToTop.tsx
+++ components/basecomponents/ScrollToTop.tsx
@@ -1,16 +1,31 @@
+import { useScrollPosition } from '@n8tb1t/use-scroll-position';
 import ChevronUp from 'public/icons/chevron-up.svg';
+import { useCallback, useState } from 'react';
 
 export const ScrollToTop = () => {
-  const handleScrollTop = () => {
+  const handleScrollTop = useCallback(() => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: 'smooth',
     });
-  };
+  }, []);
+
+  const [hideButton, setHideButton] = useState(true);
+
+  useScrollPosition(({ currPos }) => {
+    if (currPos.y < 0) {
+      setHideButton(false);
+    } else {
+      setHideButton(true);
+    }
+  });
 
   return (
-    <button onClick={handleScrollTop} className="fixed  bg-white bottom-6 right-6 cursor-pointer shadow-around p-2 rounded-md">
+    <button
+      onClick={handleScrollTop}
+      className={`fixed bg-white bottom-6 right-6 cursor-pointer shadow-around p-2 rounded-md duration-300 ${hideButton && ' scale-0'}`}
+    >
       <ChevronUp />
     </button>
   );
