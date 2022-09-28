e91e4c1fad047084ed199c79ff32faf4aef351fa
Juraj Uhlar <juraj.uhlar@flowmon.com>
2022-09-28 15:17:59 +0100

    Add some console logs

--- pages/alphabet/pdf.tsx
+++ pages/alphabet/pdf.tsx
@@ -5,6 +5,7 @@ import { componentToPDFBuffer } from '../../components/pdfs/utils/reactToPdf';
 import PdfLayout from '../../components/pdfs/utils/PdfLayout';
 import { AlphabetDataObject, fetchAlphabetMain, fetchAlphabetUk } from '../../utils/getDataUtils';
 import { Language } from '../../utils/locales';
+import { rest } from 'cypress/types/lodash';
 
 class PDF extends React.Component {
   static async getInitialProps({ res, query, locale }: NextPageContext) {
@@ -16,21 +17,26 @@ class PDF extends React.Component {
     }
 
     if (res) {
-      const buffer = await componentToPDFBuffer(
-        <PdfLayout>
-          <AlphabetPdf locale={locale as Language} alphabet={alphabet} />
-        </PdfLayout>
-      );
+      try {
+        const buffer = await componentToPDFBuffer(
+          <PdfLayout>
+            <AlphabetPdf locale={locale as Language} alphabet={alphabet} />
+          </PdfLayout>
+        );
 
-      // with this header, your browser will prompt you to download the file
-      // without this header, your browse will open the pdf directly
-      // res.setHeader('Content-disposition', 'attachment; filename="article.pdf');
+        // with this header, your browser will prompt you to download the file
+        // without this header, your browse will open the pdf directly
+        res.setHeader('Content-disposition', 'attachment; filename="article.pdf');
 
-      // set content type
-      res.setHeader('Content-Type', 'application/pdf');
+        // set content type
+        res.setHeader('Content-Type', 'application/pdf');
 
-      // output the pdf buffer. once res.end is triggered, it won't trigger the render method
-      res.end(buffer);
+        // output the pdf buffer. once res.end is triggered, it won't trigger the render method
+        res.end(buffer);
+      } catch (e) {
+        console.log(e);
+        res.end({ error: e });
+      }
     }
   }
 }
