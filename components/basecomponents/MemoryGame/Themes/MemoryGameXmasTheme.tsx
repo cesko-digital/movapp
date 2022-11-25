5327389fc26dd2bcf27a61415b3a16db8bc928c6
Marek Mego <marekmego84@gmail.com>
2022-11-25 12:55:44 +0100

    music added

--- components/basecomponents/MemoryGame/Themes/MemoryGameXmasTheme.tsx
+++ components/basecomponents/MemoryGame/Themes/MemoryGameXmasTheme.tsx
@@ -7,7 +7,7 @@ const gameData = {
   audio: {
     cardFlipSound: '/kids/memory-game/card_flip.mp3',
     cardsMatchSound: '/kids/memory-game/xmasbell.mp3',
-    winMusic: '/kids/memory-game/win_music_sh.mp3',
+    winMusic: '/kids/memory-game/jingle_bells.mp3',
   },
   styles,
   cardBackImage: '/kids/memory-game/xmascard.png',
