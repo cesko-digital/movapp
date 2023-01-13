b2149babb70c3ae580d4645905e3ca5cf9b190f3
Marek Mego <marekmego84@gmail.com>
2023-01-13 09:58:15 +0100

    global state

--- components/basecomponents/MemoryGame/MemoryGameLocal.tsx
+++ components/basecomponents/MemoryGame/MemoryGameLocal.tsx
@@ -101,113 +101,39 @@ export type Theme = {
   cardsData: CardData[];
 };
 
-interface SelectedCards {
-  first: Card | null;
-  second: Card | null;
-}
-
 interface MemoryGameProps {
   theme: Theme;
 }
 
-const createUseGame = () => {
-  interface State {
-    theme: Theme;
-    cards: Card[];
-    selectedCards: SelectedCards;
-    scene: Scene;
-    controlsEnabled: boolean;
-  }
-
-  let state: State | null = null;
+const MemoryGame = ({ theme }: MemoryGameProps) => {
+  const { playCardPhrase, playPhraseRandomLang } = usePlayPhrase();
+  const { t } = useTranslation();
+  const [cards, setCards] = useState<Card[]>([]);
+  const { audio, image, styles, cardsData } = theme;
   
-  let playCardPhrase: ((card: Card) => Promise<void>) = () => Promise.resolve();
-  let playPhraseRandomLang: ((phrase: TranslationJSON) => Promise<void>) = () => Promise.resolve();
-
-  const setPlayPhrase = (playPhrase:{playCardPhrase: (card: Card) => Promise<void>,playPhraseRandomLang: (phrase: TranslationJSON) => Promise<void>}) => {
-    playCardPhrase = playPhrase.playCardPhrase;
-    playPhraseRandomLang = playPhrase.playPhraseRandomLang;
+  interface SelectedCards {
+    first: Card | null;
+    second: Card | null;
   }
 
-  const initializeState = (initState: State) => {
-    console.log('initialize state');
-    state = initState;
-    console.log(state);
-  };
+  const [selectedCards, setSelectedCards] = useState<SelectedCards>({ first: null, second: null });
 
-  const setState = (func: (state: State) => State) => {
-    console.log('update state');
-    if (state === null) throw new Error('state not initialized');
-    state = { ...state, ...func(state) };
-    console.log(state);
-  };
+  const isSelected = (card: Card) =>
+    (selectedCards.first !== null && card.id === selectedCards.first.id) ||
+    (selectedCards.second !== null && card.id === selectedCards.second.id);
 
-  const getState = () => {
-    if (state === null) throw new Error('state not initialized');
-    return state;
-  };
-
-  const [setTimer, clearTimers] = createTimer();
-
-  const isSelected = (card: Card) => {
-    const { selectedCards } = getState();
-    return (
-      (selectedCards.first !== null && card.id === selectedCards.first.id) ||
-      (selectedCards.second !== null && card.id === selectedCards.second.id)
-    );
-  };
-
-  const setCards = (cards: Card[]) => {
-    setState((state) => ({ ...state, cards }));
-  };
-
-  const setScene = (scene: Scene) => {
-    setState((state) => ({ ...state, scene }));
-  };
-
-  const setSelectedCards = (selectedCards: SelectedCards) => {
-    setState((state) => ({ ...state, selectedCards }));
-  };
-
-  const disableControls = () => {
-    setState((state) => ({ ...state, controlsEnabled: false }));
-  };
-
-  const enableControls = () => {
-    setState((state) => ({ ...state, controlsEnabled: true }));
-  };
+  const [scene, setScene] = useState<Scene>(Scene.init);
+  const [controlsDisabled, setControlsDisabled] = useState<boolean>(true);
+  const [setTimer, clearTimers] = useMemo(createTimer, []);
 
   const flipCard = (cardToFlip: Card) => {
-    console.log('flipp card');
-    setCards(getState().cards.map((card) => (card.id === cardToFlip.id ? { ...card, flipped: !card.flipped } : card)));
-  };
-
-  const initGame = (theme: Theme) => {
-    console.log('init game');
-    // init/replace game state with new data
-    const cards = createCardDeck(theme.cardsData);
-
-    const initState = {
-      theme,
-      cards,
-      selectedCards: { first: null, second: null },
-      scene: Scene.init,
-      controlsEnabled: false,
-    };
-
-    initializeState(initState);
-  };
-
-  const handleNewGameClick = () => {
-    console.log('new game click');
-    transitions[Transition.restart]();
+    setCards((cards) => cards.map((card) => (card.id === cardToFlip.id ? { ...card, flipped: !card.flipped } : card)));
   };
 
   const selectCard = (card: Card) => {
-    const state = getState();
-    if (!state.controlsEnabled || isSelected(card) || card.flipped) return;
+    if (controlsDisabled || isSelected(card) || card.flipped) return;
 
-    const { first, second } = state.selectedCards;
+    const { first, second } = selectedCards;
     // select first card
     if (first === null && !card.flipped) {
       transitions[Transition.firstCardSelected](card);
@@ -220,25 +146,24 @@ const createUseGame = () => {
   const transitions = {
     begin: () => {
       setScene(Scene.begin);
-      disableControls();
-      updateUI();
+      setControlsDisabled(true);
+      setCards(createCardDeck(cardsData));
+      setSelectedCards({ first: null, second: null });
       setTimer(() => {
         transitions[Transition.game]();
       }, 1000);
     },
     game: () => {
       setScene(Scene.game);
-      enableControls();
-      updateUI();
+      setControlsDisabled(false);
     },
     firstCardSelected: async (card: Card) => {
-      disableControls();
+      setControlsDisabled(true);
       setSelectedCards({ first: card, second: null });
-      setScene(Scene.firstCardSelected);      
+      setScene(Scene.firstCardSelected);
       // play css animations and sounds
       flipCard(card); // 0.3s
-      updateUI();
-      await playAudio(getState().theme.audio.cardFlipSound);
+      await playAudio(audio.cardFlipSound);
       playCardPhrase(card);
 
       setTimer(() => {
@@ -246,13 +171,12 @@ const createUseGame = () => {
       }, 600); // reduced for fastrer UX
     },
     secondCardSelected: async ({ first, second }: { first: Card; second: Card }) => {
-      disableControls();
+      setControlsDisabled(true);
       setSelectedCards({ first, second });
-      setScene(Scene.secondCardSelected);      
+      setScene(Scene.secondCardSelected);
       // play css animations and sounds
       flipCard(second); // 0.3s
-      updateUI();
-      await playAudio(getState().theme.audio.cardFlipSound);
+      await playAudio(audio.cardFlipSound);
       await playCardPhrase(second);
 
       const cardsMatch = first.image === second.image;
@@ -265,15 +189,13 @@ const createUseGame = () => {
     },
     cardsMatch: async () => {
       setScene(Scene.cardsMatch);
-      updateUI();
       delay(100);
-      await playAudio(getState().theme.audio.cardsMatchSound);
+      await playAudio(audio.cardsMatchSound);
       setScene(Scene.cardsMatchReward);
-      updateUI();
       Math.random() > 0.5 && (await playPhraseRandomLang(getRandomElement(phrases.good)));
       // reset selected cards
       // check win
-      if (getState().cards.every((card) => card.flipped)) {
+      if (cards.every((card) => card.flipped)) {
         transitions[Transition.win]();
       } else {
         setSelectedCards({ first: null, second: null });
@@ -282,111 +204,46 @@ const createUseGame = () => {
     },
     cardsDontMatch: async ({ first, second }: { first: Card; second: Card }) => {
       setScene(Scene.cardsDontMatch);
-      updateUI();
       await delay(1000);
       Math.random() > 0.8 && (await playPhraseRandomLang(getRandomElement(phrases.wrong)));
       flipCard(first);
       flipCard(second);
-      updateUI();
-      await playAudio(getState().theme.audio.cardFlipSound);
+      await playAudio(audio.cardFlipSound);
       setSelectedCards({ first: null, second: null });
       transitions[Transition.game]();
     },
     win: async () => {
       setScene(Scene.win);
-      updateUI();
       await playPhraseRandomLang(getRandomElement(phrases.win));
       await delay(200);
       setScene(Scene.winReward);
-      updateUI();
-      playAudio(getState().theme.audio.winMusic);
+      playAudio(audio.winMusic);
     },
     restart: async () => {
-      disableControls();
+      setControlsDisabled(true);
       playPhraseRandomLang(getRandomElement(phrases.newGame));
       setScene(Scene.goNewGame);
-      updateUI();
       clearTimers();
       setTimer(() => {
-        initGame(getState().theme);
         transitions[Transition.begin]();
       }, 500);
     },
   };
 
-  interface Controls {
-    selectCard: (card: Card) => void;
-    isSelected: (card: Card) => boolean;
-    handleNewGameClick: () => void;
-  }
-
-  const controls = { selectCard, isSelected, handleNewGameClick };
-
-  interface Game {
-    state: State;
-    controls: Controls;
-  }
-
-  const getGame = () => (state === null ? null : { state, controls });
-
-  let uiCallback: null | (() => void) = null;
-  const connectUI = (func: () => void) => {
-    console.log('connect ui');
-    uiCallback = func;
-  };
-  const disconnectUI = () => (uiCallback = null);
-  const updateUI = () => {
-    if (uiCallback !== null) {
-      console.log('update ui');
-      uiCallback();
-    }
-  };
-
-  const useGame = (theme: Theme) => {
-    const [game, setGame] = useState<Game | null>(null);
-    const { playCardPhrase, playPhraseRandomLang } = usePlayPhrase();
-
-    useEffect(() => {
-      // connect ui -> setGame()
-      setPlayPhrase({ playCardPhrase, playPhraseRandomLang });
-      connectUI(() => setGame(getGame()));
-      return () => {
-        // disconnect ui
-        disconnectUI();
-      };
-    }, []);
-
-    useEffect(() => {
-      // init with new data
-      initGame(theme);
-      transitions[Transition.begin]();
-      // start new game maybe
-    }, [theme]);
-
-    return game;
-  };
-
-  return useGame;
-};
-
-const useGame = createUseGame();
-
-const MemoryGame = ({ theme }: MemoryGameProps) => {  
-  const { t } = useTranslation();
-  
-  const game = useGame(theme);
-  if (game === null) return null;
-
-  const { scene, cards } = game.state;
-  const { image, styles } = game.state.theme;
-  const { isSelected, selectCard, handleNewGameClick } = game.controls;
+  useEffect(() => {
+    console.log('useEffect theme fired');
+    transitions[Transition.begin]();
+    // clean up
+    return () => {
+      clearTimers();
+    };
+  }, [theme,clearTimers]);
 
   return (
     <div className={styles.app}>
-      <Button className={styles.newGameButton} text={t('utils.new_game')} onClick={handleNewGameClick} />
+      <Button className={styles.newGameButton} text={t('utils.new_game')} onClick={transitions[Transition.restart]} />
       <div className={styles.board}>
         {scene !== Scene.init &&
-          cards !== null &&
           cards.map((card) => (
             <Card
               key={card.id}
