11d9d39735bdec6cde64c32f135041f2bfbd6395
Filip Tatíček <filtaticek@seznam.cz>
2023-04-02 12:35:47 +0200

    Now everything works as it should, fixing the problem with ukranian website

--- components/temporarycomponents/EasterBanner.tsx
+++ components/temporarycomponents/EasterBanner.tsx
@@ -8,13 +8,14 @@ import Link from 'next/link';
 
 const EasterBanner = () => {
   const { t } = useTranslation();
-  const link = t(`easter_banner.box_easter_kids_anchor.${getCountryVariant()}`);
+  const dictionaryLinkTranslation = t(`easter_banner.box_easter_dictionary_anchor.${getCountryVariant()}`);
+  const kidsLinkTranslation = t(`easter_banner.box_easter_kids_anchor.${getCountryVariant()}`);
 
   return (
     <div className="max-w-7xl bg-lime-500 p-4 sm:p-8 md:p-12 shadow-xxl mt-[-6rem] mb-32">
       <div className="grid md:grid-cols-3 gap-8">
         <div className="justify-self-center my-4 md:my-0">
-          <Link href={`/dictionary#${t('easter_banner.box_easter_dictionary_anchor')}`}>
+          <Link href={`/dictionary#${dictionaryLinkTranslation}`}>
             <a>
               <Image src={ColouredEggs} alt="Velikonoční vajíčka." width={256} height={194} />
             </a>
@@ -23,7 +24,7 @@ const EasterBanner = () => {
         <div className="homepage-box w-full group hover:text-primary-blue    pr-4 md:border-r-1 md:border-r-solid md:border-lime-400">
           <DictionaryIcon className="w-6 mb-2 group-hover:fill-primary-red" />
           <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">
-            <Link href={`/dictionary#${t('easter_banner.box_easter_dictionary_anchor')}`}>
+            <Link href={`/dictionary#${dictionaryLinkTranslation}`}>
               <a>{t('easter_banner.box_easter_dictionary_title')}</a>
             </Link>
           </h2>
@@ -32,7 +33,7 @@ const EasterBanner = () => {
         <div className="homepage-box w-full group hover:text-primary-blue    pr-4">
           <EggsIcon className="w-6 mb-2 group-hover:fill-primary-red" />
           <h2 className="text-lg mt-0 pb-1 inline-block border-b-1 border-b-solid border-b-primary-black">
-            <Link href={`/kids#${link}`}>
+            <Link href={`/kids#${kidsLinkTranslation}`}>
               <a>{t('easter_banner.box_easter_kids_title')}</a>
             </Link>
           </h2>
