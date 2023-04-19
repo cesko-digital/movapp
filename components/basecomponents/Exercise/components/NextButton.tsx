79ab8bc35447bd9edd437a40e5df63758f5ab5cc
filiptaticek <96868437+filiptaticek@users.noreply.github.com>
2023-04-19 21:17:39 +0200

    Filiptaticek (#271)
    * Adding new translations to locales
    
    * Adding the new translations to components
    
    * Fixing the czech translation
    
    ---------
    
    Co-authored-by: Filip Tatíček <filtaticek@seznam.cz>

--- components/basecomponents/Exercise/components/NextButton.tsx
+++ components/basecomponents/Exercise/components/NextButton.tsx
@@ -2,6 +2,7 @@ import { Exercise } from '../exerciseStore';
 import { useRef } from 'react';
 import { Button } from './Button';
 import { animation } from '../utils/animation';
+import { useTranslation } from 'react-i18next';
 
 interface NextButtonProps {
   onClick: Exercise['next'];
@@ -9,12 +10,14 @@ interface NextButtonProps {
 
 export const NextButton = ({ onClick }: NextButtonProps) => {
   const btnRef = useRef(null);
+  const { t } = useTranslation();
+
   return (
     <div className="flex mb-3">
       <Button
         ref={btnRef}
         className=""
-        text="Další"
+        text={t('utils.next') || ''}
         onClick={async () => {
           if (btnRef.current === null) return;
           await animation.select(btnRef.current).finished;
