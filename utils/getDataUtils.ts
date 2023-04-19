39797b043f1ccd139101302850a2986a946b0d7c
Martin Starosta <martin.starosta@labzone.tech>
2023-04-19 12:15:19 +0200

    Refactor

--- utils/getDataUtils.ts
+++ utils/getDataUtils.ts
@@ -1,6 +1,8 @@
-import { CountryVariant } from './locales';
-import { getCountryVariant, Language } from '../utils/locales';
+import { getCountryVariant, Language, CountryVariant } from 'utils/locales';
 import fetch from 'node-fetch';
+
+const BASE_API_URL = process.env.BASE_API_URL || 'https://data.movapp.eu';
+const KIDS_CATEGORY_ID = 'recSHyEn6N0hAqUBp';
 
 /**
  * Dictionary
@@ -54,45 +56,31 @@ export class Phrase {
   }
 
   getTranslation = (language?: Language) => {
-    if (language === 'uk') {
-      return this.phraseData.source.translation;
-    } else {
-      return this.phraseData.main.translation;
-    }
+    return language === 'uk' ? this.phraseData.source.translation : this.phraseData.main.translation;
   };
 
   getTranscription = (language?: Language) => {
-    if (language === 'uk') {
-      return this.phraseData.source.transcription;
-    } else {
-      return this.phraseData.main.transcription;
-    }
+    return language === 'uk' ? this.phraseData.source.transcription : this.phraseData.main.transcription;
   };
 
   getSoundUrl = (language?: Language) => {
-    if (language === 'uk') {
-      return this.phraseData.source.sound_url;
-    } else {
-      return this.phraseData.main.sound_url;
-    }
+    return language === 'uk' ? this.phraseData.source.sound_url : this.phraseData.main.sound_url;
   };
 
-  getImageUrl = () => this.phraseData.image_url;
+  getImageUrl = (): string | null => this.phraseData.image_url;
   getId = () => this.phraseData.id;
 }
 
-const KIDS_CATEGORY_ID = 'recSHyEn6N0hAqUBp';
+const mapPhraseObjectsToPhrases = (phraseObjects: PhraseDataObject[]): Phrase[] => {
+  return phraseObjects.map((phraseObject) => new Phrase(phraseObject));
+};
 
-export const parseCategory = (categoryObject: CategoryDataObject, dictionaryObject: DictionaryDataObject): Category => {
+export const parseCategory = ({ id, name, phrases }: CategoryDataObject, dictionaryObject: DictionaryDataObject): Category => {
   return {
-    id: categoryObject.id,
-    nameMain: categoryObject.name.main,
-    nameUk: categoryObject.name.source,
-    translations: categoryObject.phrases
-      .map((phraseId) => dictionaryObject.phrases[phraseId])
-      // Some phrases might be missing for some language variants
-      .filter(Boolean)
-      .map((phrase) => new Phrase(phrase)),
+    id,
+    nameMain: name.main,
+    nameUk: name.source,
+    translations: mapPhraseObjectsToPhrases(phrases.map((phraseId) => dictionaryObject.phrases[phraseId]).filter(Boolean)),
   };
 };
 
@@ -100,16 +88,12 @@ export const getCategories = (dictionaryObject: DictionaryDataObject): Category[
   dictionaryObject.categories.map((category) => parseCategory(category, dictionaryObject));
 
 export const getAllPhrases = (dictionaryObject: DictionaryDataObject): Phrase[] => {
-  return [...Object.values(dictionaryObject.phrases)].map((phraseObject) => new Phrase(phraseObject));
+  return mapPhraseObjectsToPhrases([...Object.values(dictionaryObject.phrases)]);
 };
 
 export const getKidsCategory = (dictionaryObject: DictionaryDataObject): Category | undefined => {
   const categoryObject = dictionaryObject.categories.find((category) => category.id === KIDS_CATEGORY_ID);
-  if (!categoryObject) {
-    return undefined;
-  } else {
-    return parseCategory(categoryObject, dictionaryObject);
-  }
+  return categoryObject ? parseCategory(categoryObject, dictionaryObject) : undefined;
 };
 
 export const getPhraseById = (dictionaryObject: DictionaryDataObject, phraseId: string): Phrase => {
@@ -117,16 +101,13 @@ export const getPhraseById = (dictionaryObject: DictionaryDataObject, phraseId: 
 };
 
 export const fetchRawDictionary = async (country?: CountryVariant): Promise<DictionaryDataObject> => {
-  const response = await fetch(`https://data.movapp.eu/uk-${country ?? getCountryVariant()}-dictionary.json`);
-  const json = (await response.json()) as DictionaryDataObject;
-  return json;
+  const response = await fetch(`${BASE_API_URL}/uk-${country ?? getCountryVariant()}-dictionary.json`);
+  return (await response.json()) as DictionaryDataObject;
 };
 
 export const fetchDictionary = async (country?: CountryVariant): Promise<DictionaryDataObject> => {
   const json = await fetchRawDictionary(country);
-  // filter out hidden categories
-  const result = { ...json, categories: json.categories.filter((category) => !category.hidden) };
-  return result;
+  return { ...json, categories: json.categories.filter((category) => !category.hidden) };
 };
 
 /**
@@ -149,12 +130,12 @@ export interface AlphabetDataObject {
   data: Letter[];
 }
 
-export const fetchAlphabetUk = async () => {
-  const result = await (await fetch(`https://data.movapp.eu/uk-${getCountryVariant()}-alphabet.json`)).json();
-  return result as AlphabetDataObject;
+export const fetchAlphabetUk = async (): Promise<AlphabetDataObject> => {
+  const response = await fetch(`${BASE_API_URL}/uk-${getCountryVariant()}-alphabet.json`);
+  return await response.json();
 };
 
-export const fetchAlphabetMain = async () => {
-  const result = await (await fetch(`https://data.movapp.eu/${getCountryVariant()}-uk-alphabet.json`)).json();
-  return result as AlphabetDataObject;
+export const fetchAlphabetMain = async (): Promise<AlphabetDataObject> => {
+  const response = await fetch(`${BASE_API_URL}/${getCountryVariant()}-uk-alphabet.json`);
+  return (await response.json()) as AlphabetDataObject;
 };
