5f5a130a389d734c3490b390b2eb141e8ea92aaa
Martina Zifcakova <zifcakova.martina@gmail.com>
2023-05-09 09:40:50 +0200

    Updates of Home page tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,20 +5,44 @@ describe('Test Movapp - Home page', () => {
     cy.visit('/');
   });
 
-  it('it renders', () => {
+  it('Test that page has correct h1', () => {
     cy.get('h1').should('contain.text', 'Naučte se základy češtiny a ukrajinštiny pro běžné životní situace');
-    cy.title().should('be.eq', 'Movapp.cz – aby se Češi a Ukrajinci snadno domluvili');
+    cy.get('h1').invoke('text').should('not.be.empty') 
+  });
 
-    // Hlavna karta s odkazmi
-    cy.contains('Slovníček').should('be.visible');
-    cy.contains('Pro děti').should('be.visible');
-    cy.contains('Wiki').should('be.visible');
-    cy.contains('Movapp je');
-    cy.contains('Ukrajinská abeceda').should('be.visible');
+  it('Test that page has a correct title', () => {
+    cy.title().should("be.eq", "Movapp.cz – aby se Češi a Ukrajinci snadno domluvili");
+  });
 
-    // Lokalizacie
+   it('Test that Nav bar has a correct sections', () => {
+    cy.contains('header a', 'Abeceda').invoke('attr', 'href').should('equal', '/alphabet')
+    cy.contains('header a', 'Slovníček').invoke('attr', 'href').should('equal', '/dictionary')
+    cy.contains('header button', 'Pro děti').invoke('text').should('equal', 'Pro děti');
+    cy.contains('header a', 'Wiki').invoke('attr', 'href').should('equal', '/wiki');
+    cy.contains('header a', 'O nás').invoke('attr', 'href').should('equal', '/about');
+    cy.contains('header a', 'Kontakty').invoke('attr', 'href').should('equal', '/contacts');
+  });
+
+  it('Test visibility of the elements in Header', () => {
+    cy.contains('header a', 'Abeceda').should('be.visible');
+    cy.contains('header a', 'Slovníček').should('be.visible');
+    cy.contains('header button', 'Pro děti').should('be.visible');
+    cy.contains('header a', 'Wiki').should('be.visible');
+    cy.contains('header a', 'O nás').should('be.visible');
+    cy.contains('header a', 'Kontakty').should('be.visible');
+  });
+
+it('Test body part of the page', () => {
+    cy.contains('h2', 'Pro děti').should('be.visible');
+    cy.contains('h2', 'Wiki').should('be.visible');
+    cy.contains('h2', 'Movapp je').should('be.visible');
+    cy.contains('h2', 'Ukrajinská abeceda').should('be.visible');
+  });
+
+it('Test of footer of the page', () => {
     cy.contains('Movapp mluví dalšími jazyky').should('be.visible');
     cy.contains('Slovensky').should('be.visible');
-    cy.contains('Polski').should('be.visible');
+    cy.contains('Polski').should('be.visible')
+    
   });
-});
+})
