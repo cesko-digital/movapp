67b0182f26e0348be2044107adc101d9e4178862
Martin Starosta <martin.starosta@labzone.tech>
2023-06-07 18:38:15 +0200

    Viewport

--- pages/kiosk/KioskLayout.tsx
+++ pages/kiosk/KioskLayout.tsx
@@ -5,7 +5,7 @@ const KioskLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       </Head>
       <div className="bg-gradient-to-r from-[#FDF6D2] to-[#99BDE4] h-screen py-[57px] overflow-hidden">{children}</div>;
     </>
