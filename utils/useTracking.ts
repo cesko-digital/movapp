bfe2322d017faca624b13efbf964328bca63464e
Filip Tatíček <filtaticek@seznam.cz>
2023-07-02 18:30:13 +0200

    Preventing sending the data to Plausible twice at the end of the exercise

--- utils/useTracking.ts
+++ utils/useTracking.ts
@@ -12,17 +12,17 @@ export const useTracking = () => {
   const plausible = usePlausible();
   const lang = useLanguage();
 
-  const hasBeenAlreadyRef = useRef(false); //Use useRef to prevent sending data to Plausible twice because of the double renders
+  const hasBeenAlreadyRef = useRef(false); // Use useRef to prevent sending data to Plausible twice because of the double renders
 
   useEffect(() => {
     if (status === 'active' && history.length === 0 && !hasBeenAlreadyRef.current) {
       console.log(`Exercise is started, sending to Plausible`);
       plausible('Exercise-Started', { props: { language: lang.currentLanguage, length: size } });
-      hasBeenAlreadyRef.current = true; //For one second, we prevent sending the data
+      hasBeenAlreadyRef.current = true; // For one second, we prevent sending the data
       setTimeout(() => {
-        hasBeenAlreadyRef.current = false; //After one second, we allow sending the data again
+        hasBeenAlreadyRef.current = false; // After one second, we allow sending the data again
       }, 1000);
-    } else if (history.length === size) {
+    } else if (history.length === size && status === 'completed') {
       console.log(`Exercise is finished, sending to Plausible`);
       const correctAnswers = history.filter((exercise) => {
         return exercise.status === ExerciseStatus.completed && exercise.result?.score === 100;
