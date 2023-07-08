736a73d3f86e120b5fb56ca43ebe7c6b658c16ed
Jakub Hašek <28836407+kubahasek@users.noreply.github.com>
2023-07-08 10:43:03 +0200

    fixed ios spinner error by adding style to spinner

--- components/basecomponents/Feedback.tsx
+++ components/basecomponents/Feedback.tsx
@@ -26,10 +26,10 @@ const Feedback = () => {
         </button>
       </div>
       <Modal closeModal={() => setModalOpen(false)} isOpen={modalOpen} unmount>
-        <div className={`absolute flex justify-center items-center w-full h-full -z-10 top-0 left-0`}>
-          <Spinner />
+        <div className="absolute flex justify-center items-center w-full h-full -z-50 top-0 left-0">
+          <Spinner style={{ zIndex: -50 }} />
         </div>
-        <iframe className="airtable-embed" src={iframeSrc} frameBorder="0" width="100%" height="533" />
+        <iframe className="airtable-embed z-50" src={iframeSrc} frameBorder="0" width="100%" height="533" />
       </Modal>
     </>
   );
