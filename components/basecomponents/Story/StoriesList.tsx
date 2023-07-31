56a8d8ef935544d41a0f7e3436f641899a3baf19
Filip Tatíček <filtaticek@seznam.cz>
2023-07-31 17:11:39 +0200

    New format of stories, using grid, stories are no more divided on czech and ukranian, but are all together

--- components/basecomponents/Story/StoriesList.tsx
+++ components/basecomponents/Story/StoriesList.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslation } from 'next-i18next';
-
 import { CountryVariant, getCountryVariant } from 'utils/locales';
+import PlayIcon from 'public/icons/stories-play.svg';
 
 /** Components */
 import StoryCard from './StoryCard';
@@ -24,40 +24,23 @@ const StoriesList = ({ stories }: StoriesListProps) => {
   // We support only stories if there is title translated to current language
   const filteredStories = stories.filter((story) => story.title[countryVariant]);
 
-  // We want to have list of Ukrainian stories, then other stories
-  const storiesUA = filteredStories.filter((story) => story.country === 'UA');
-  const storiesCEE = filteredStories.filter((story) => story.country === 'CZ');
-
   const RenderStoryPanel = ({ story }: { story: Story }) => (
-    <div className="h-42 m-auto my-8 flex bg-slate-50 rounded-2xl sm:w-3/5 xl:w-2/5 xxl:w-1/3" key={story.slug}>
-      <div className="relative w-[105px] lg:h-[200px] lg:w-[300px]">
-        <Image src={`/kids/${story.slug}.jpg`} fill className="rounded-l-2xl" alt={story.title[currentLanguage]} />
-      </div>
-      <div className="flex items-center xl:ml-6 w-full">
-        <div className="w-3/5">
-          <p className="my-4 mx-4 md:mx-8 text-sm lg:text-xl ">{story.title[currentLanguage]}</p>
-          <div className="flex items-center mb-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 mx-2 md:ml-8 text-sm lg:text-xl"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <span className="text-sm lg:text-xl">{story.duration}</span>
-          </div>
-        </div>
-
-        <Link
-          href={`/kids/stories/${story.slug}`}
-          className="w-2/5 bg-primary-blue rounded-2xl text-white text-[10px] sm:text-[12px] lg:text-base p-2 w-3/5 text-center mr-4"
-        >
-          {t('kids_page.playStory')}
+    <div className="w-[342px] h-[376px] mb-[32px] sm:w-[400px] sm:h-[400px] sm:mb-[48px] group">
+      <div className="rounded-t-2xl relative h-[300px]">
+        <Image
+          src={`/kids/${story.slug}.jpg`}
+          width={400}
+          height={300}
+          className="w-full h-full object-cover rounded-t-2xl"
+          alt={story.title[currentLanguage]}
+        />
+        <Link href={`/kids/stories/${story.slug}`} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <PlayIcon className="w-full cursor-pointer w-[60px] h-[60px] sm:w-[74.57px] sm:h-[74.57px] group-hover:visible xl:invisible inline-block active:scale-125 active:fill-primary-yellow active:stroke-primary-yellow transition duration-100 fill-primary-blue stroke-primary-blue" />
         </Link>
       </div>
+      <p className="bg-[#FFFFFF] text-primary-blue text-center text-2xl rounded-b-2xl flex justify-center text-center h-[76px] sm:h-[100px] items-center">
+        {story.title[currentLanguage]}
+      </p>
     </div>
   );
 
@@ -85,20 +68,19 @@ const StoriesList = ({ stories }: StoriesListProps) => {
             </div>
           </div>
         ) : (
-          <>
-            <h2 className={`text-primary-blue text-center`}>{t(titleKey)}</h2>
-            {localizedStories.map((story) => (
-              <RenderStoryPanel key={story.slug} story={story} />
-            ))}
-          </>
+          <div className="grid grid-cols-1 min-[900px]:grid-cols-2 xl:grid-cols-3 justify-items-center">
+            {localizedStories.map((story) => {
+              return <RenderStoryPanel key={story.slug} story={story} />;
+            })}
+          </div>
         )}
       </>
     );
   };
   return (
-    <div className="flex flex-col">
-      {storiesCEE.length > 0 && <RenderStoriesSection language="cs" stories={storiesCEE} titleKey="kids_page.czechStories" />}
-      {storiesUA.length > 0 && <RenderStoriesSection language="uk" stories={storiesUA} titleKey="kids_page.ukrainianStories" />}
+    <div className="flex flex-col max-w-[1296px] m-auto">
+      <p className={'text-[30px] sm:text-[38px] text-primary-blue text-center mb-[32px] sm:mb-[43px]'}>{t('header.forkids_stories')}</p>
+      {filteredStories.length > 0 && <RenderStoriesSection language="cs" stories={filteredStories} titleKey="kids_page.czechStories" />}
     </div>
   );
 };
