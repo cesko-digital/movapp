bf0244b0c0360be7aec8c914497448c915e9712e
Filip Tatíček <filtaticek@seznam.cz>
2023-08-01 16:22:34 +0200

    Fixing responsivity and loading time of the page

--- components/basecomponents/Story/StoriesList.tsx
+++ components/basecomponents/Story/StoriesList.tsx
@@ -24,25 +24,31 @@ const StoriesList = ({ stories }: StoriesListProps) => {
   // We support only stories if there is title translated to current language
   const filteredStories = stories.filter((story) => story.title[countryVariant]);
 
-  const RenderStoryPanel = ({ story }: { story: Story }) => (
-    <div className="w-[342px] h-[376px] mb-[32px] sm:w-[400px] sm:h-[400px] sm:mb-[48px] group">
-      <div className="rounded-t-2xl relative h-[300px]">
-        <Image
-          src={`/kids/${story.slug}.jpg`}
-          width={400}
-          height={300}
-          className="w-full h-full object-cover rounded-t-2xl"
-          alt={story.title[currentLanguage]}
-        />
-        <Link href={`/kids/stories/${story.slug}`} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <PlayIcon className="w-full cursor-pointer w-[60px] h-[60px] sm:w-[74.57px] sm:h-[74.57px] group-hover:visible xl:invisible inline-block active:scale-125 active:fill-primary-yellow active:stroke-primary-yellow transition duration-100 fill-primary-blue stroke-primary-blue" />
-        </Link>
+  const RenderStoryPanel = ({ story }: { story: Story }) => {
+    // Check if the current image's source is "/kids/dvanact-mesicku.jpg", because it takes a lot of time to load
+    const isPriorityImage = story.slug === 'dvanact-mesicku';
+
+    return (
+      <div className="max-w-[342px] sm:max-w-none h-[376px] mb-[32px] sm:w-[400px] sm:h-[400px] sm:mb-[48px] group">
+        <div className="rounded-t-2xl relative h-[300px]">
+          <Image
+            src={`/kids/${story.slug}.jpg`}
+            width={400}
+            height={300}
+            className="w-full h-full object-cover rounded-t-2xl"
+            alt={story.title[currentLanguage]}
+            priority={isPriorityImage}
+          />
+          <Link href={`/kids/stories/${story.slug}`} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <PlayIcon className="w-full cursor-pointer w-[60px] h-[60px] sm:w-[74.57px] sm:h-[74.57px] group-hover:visible xl:invisible inline-block active:scale-125 active:fill-primary-yellow active:stroke-primary-yellow transition duration-100 fill-primary-blue stroke-primary-blue" />
+          </Link>
+        </div>
+        <p className="bg-[#FFFFFF] text-primary-blue text-center text-2xl rounded-b-2xl flex justify-center text-center h-[76px] sm:h-[100px] items-center">
+          {story.title[currentLanguage]}
+        </p>
       </div>
-      <p className="bg-[#FFFFFF] text-primary-blue text-center text-2xl rounded-b-2xl flex justify-center text-center h-[76px] sm:h-[100px] items-center">
-        {story.title[currentLanguage]}
-      </p>
-    </div>
-  );
+    );
+  };
 
   const RenderStoriesSection = ({
     language,
