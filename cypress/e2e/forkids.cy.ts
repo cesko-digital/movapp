8fc2d6d1b402a48d26f44de9933554c63dd4d01e
matuyka <zifcakova.martina@gmail.com>
2023-09-17 06:17:25 -0700

    Updated For Kids test

--- cypress/e2e/forkids.cy.ts
+++ cypress/e2e/forkids.cy.ts
@@ -9,15 +9,35 @@ describe('Test Movapp - For Kids page', () => {
         cy.get('@menu').find('~ div ul li a').contains('Slovíčka')
             .should('be.visible')
             .click()
+
+    
+    it('Page with cards is visible', () => {
+        cy.get('#panenka').should('exist').and('be.visible')
+            })
+
+        cy.contains('header nav li button', 'Pro děti').as('menu').click()
+        cy.get('@menu').find('~ div ul li a').contains('Pohádky')
+        .should('be.visible')
+        .click()
+
+        
+        cy.contains('header nav li button', 'Pro děti').as('menu').click()
+        cy.get('@menu').find('~ div ul li a').contains('Pexeso')
+            .should('be.visible')
+            .click()
+
+        cy.contains('header nav li button', 'Pro děti').as('menu').click()
+        cy.get('@menu').find('~ div ul li a').contains('Obrázkový kvíz')
+                .should('be.visible')
+                .click()
     })
 
     it('Link in menu is correctly bordered', () => {
         cy.contains('header nav li button', 'Pro děti').parents('li')
+        .should('have.class', 'border-b-2')
+        
            
     })
 
-    it('Page with cards is visible', () => {
-        cy.get('#panenka').should('exist').and('be.visible')
-    })
 })
     
